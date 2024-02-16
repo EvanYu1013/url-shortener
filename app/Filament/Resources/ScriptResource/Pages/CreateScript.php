df9f4d719785ee45a74c0d2c550083c79554976c
bleuren <bleuren421@gmail.com>
2024-02-17 04:35:36 +0800

    Add strict types declaration to script resource pages

--- app/Filament/Resources/ScriptResource/Pages/CreateScript.php
+++ app/Filament/Resources/ScriptResource/Pages/CreateScript.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\ScriptResource\Pages;
 
 use App\Filament\Resources\ScriptResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateScript extends CreateRecord
--- app/Filament/Resources/ScriptResource/Pages/ListScripts.php
+++ app/Filament/Resources/ScriptResource/Pages/ListScripts.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\ScriptResource\Pages;
 
 use App\Filament\Resources\ScriptResource;
