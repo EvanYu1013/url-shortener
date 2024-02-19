c2210fae726889af6cb7a366c540e54715359ba4
Bleu <bleuren@hotmail.com>
2024-02-20 07:37:13 +0800

    Add footer to admin panel

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer
+    class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow flex items-center justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 text-center dark:text-gray-400">
+        © 2024 <a href="#" class="hover:underline">Your Company Name</a>. All Rights Reserved.
+        Designed by <a href="https://bleu.tw" class="hover:underline font-bold">Bleu</a>.
+    </span>
+</footer>
